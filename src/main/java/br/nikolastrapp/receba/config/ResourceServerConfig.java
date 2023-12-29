a5aad47775002fc8b65a0aab52c030cc44020a0f
Nikolas Trapp <nikolastrapp@gmail.com>
2023-12-29 00:56:06 -0300

    Feat: Persistindo os clients em banco de dados, Adicioando mais assinaturas ao token e criando um converter para o resource server resgatar informações do token.

--- src/main/java/br/nikolastrapp/receba/config/ResourceServerConfig.java
+++ src/main/java/br/nikolastrapp/receba/config/ResourceServerConfig.java
@@ -6,7 +6,18 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.isNull;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -22,8 +33,25 @@ public class ResourceServerConfig {
             .and()
             .csrf().disable()
             .cors().disable()
-            .oauth2ResourceServer().jwt();
+            .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
 
         return http.formLogin(Customizer.withDefaults()).build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter((jwt) -> {
+            var authorities = jwt.getClaimAsStringList("authorities");
+            if (isNull(authorities)) {
+                return Collections.emptyList();
+            }
+            var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+            var grantedAuthorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+            var grantedUserAuthorities = authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+            grantedAuthorities.addAll(grantedUserAuthorities);
+            return grantedAuthorities;
+        });
+        return converter;
     }
 }
--- src/main/java/br/nikolastrapp/receba/repositories/UserRepository.java
+++ src/main/java/br/nikolastrapp/receba/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package br.nikolastrapp.receba.repositories;
 
 import br.nikolastrapp.receba.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
-    Optional<UserEntity> findByEmail(String email);
+    @Query("select u from users u where u.username = :username or u.email = :username ")
+    Optional<UserEntity> findByEmailOrUsername(String username);
 
 }
